05ae8ad1d7b5d06b13cf288dde6eb132caf88e9e
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-08-30 16:51:47 +0100

    92+

--- request-reciever.js
+++ request-reciever.js
@@ -3,6 +3,26 @@ const controller = require('./controller');
 const pcap = require('pcap');
 const session = pcap.createSession('wlan0', 'tcp port 9000');
 
+function filterPacket(packet) {
+  const ip = packet.payload.payload;
+  if (ip && ip.saddr && ip.daddr) {
+    const tcp = ip.payload;
+    if (tcp && tcp.data) {
+      return {
+        src: ip.saddr.addr.join('.'),
+        dst: ip.daddr.addr.join('.'),
+        payloadLength: tcp.data.length
+      };
+    }
+  }
+  return null;
+}
+
+
+
+
+
+
 session.on('packet', (rawPacket) => {
   const packet = pcap.decode.packet(rawPacket);
 
@@ -13,7 +33,9 @@ session.on('packet', (rawPacket) => {
     const tcp = ip.payload;
 
     if (ip && tcp && ip.saddr && ip.daddr && tcp.data) {
+      if(tcp.data.length > 92) {
       console.log(`From ${ip.saddr.addr.join('.')} to ${ip.daddr.addr.join('.')}, payload length: ${tcp.data ? tcp.data.length : 0}`);
+      }
     }
   }
 });
