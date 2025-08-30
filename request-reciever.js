21df18c8bae721d75c6ef0d84f603662a3307e2f
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-08-30 16:34:17 +0100

    sniff sniff?

--- request-reciever.js
+++ request-reciever.js
@@ -1,28 +1,9 @@
-const WebSocket  = require('ws');
 const controller = require('./controller');
-const net = require('net');
 
-const JAVA_APP_PORT = 9000;
-const PROXY_PORT = 9001;
-const JAVA_HOST = '127.0.0.1';
+const pcap = require('pcap');
+const session = pcap.createSession('eth0', 'tcp port 9000');
 
-// Proxy server
-const server = net.createServer((clientSocket) => {
-  console.log("Incoming connection to proxy");
-
-  // Connect to Java app
-  const javaSocket = net.connect(JAVA_APP_PORT, JAVA_HOST);
-
-  // client -> java
-  clientSocket.pipe(javaSocket);
-  // java -> client
-  javaSocket.pipe(clientSocket);
-
-  // Error handling
-  clientSocket.on('error', (err) => console.error("Client socket error:", err));
-  javaSocket.on('error', (err) => console.error("Java socket error:", err));
-});
-
-server.listen(PROXY_PORT, () => {
-  console.log(`Proxy listening on port ${PROXY_PORT}, forwarding to ${JAVA_HOST}:${JAVA_APP_PORT}`);
+session.on('packet', (rawPacket) => {
+  const packet = pcap.decode.packet(rawPacket);
+  console.log("Got packet:", JSON.stringify(packet, null, 2));
 });
