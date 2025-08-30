bfcf7e1d48c9e51d99f71405905223430d6443af
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-08-30 16:44:53 +0100

    bug fix

--- request-reciever.js
+++ request-reciever.js
@@ -7,10 +7,13 @@ session.on('packet', (rawPacket) => {
   const packet = pcap.decode.packet(rawPacket);
 
   // Example: Extract and log only source/destination IP and TCP payload length
+  
   const ip = packet.payload.payload;
-  const tcp = ip.payload;
+  if(ip != null && ip != undefined) {
+    const tcp = ip.payload;
 
-  if (ip && tcp && ip.saddr && ip.daddr) {
-    console.log(`From ${ip.saddr.addr.join('.')} to ${ip.daddr.addr.join('.')}, payload length: ${tcp.data ? tcp.data.length : 0}`);
+    if (ip && tcp && ip.saddr && ip.daddr) {
+      console.log(`From ${ip.saddr.addr.join('.')} to ${ip.daddr.addr.join('.')}, payload length: ${tcp.data ? tcp.data.length : 0}`);
+    }
   }
 });
