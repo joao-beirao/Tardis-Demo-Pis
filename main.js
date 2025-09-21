5d2d84677eba3f865e28adc1d6f2253f3de35ecc
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-21 16:07:35 +0100

    3 at once

--- main.js
+++ main.js
@@ -7,5 +7,8 @@ const PORT = 8080;
 
 getDCRAvailableEvents("p-1-1");
 
-controller.setState(0, EventStates.STATE_PENDING_INCLUDED);
+for (let i = 0; i < 3; i++) {
+     controller.setState(i, EventStates.STATE_PENDING_INCLUDED);
+    // controller.setState(i, EventStates.STATE_NOT_PENDING_INCLUDED);
+}
 
