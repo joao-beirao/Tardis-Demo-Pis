dd3bdb2e86392b0d703a2e044673eadaa842e457
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-21 16:02:48 +0100

    wtv

--- main.js
+++ main.js
@@ -8,5 +8,5 @@ const PORT = 8080;
 getDCRAvailableEvents("p-1-1");
 
 for (let i = 0; i < 3; i++) {
-    controller.setState(i, EventStates.STATE_NOT_PENDING_INCLUDED);
+    controller.setState(i, EventStates.STATE_PENDING_INCLUDED);
 }
