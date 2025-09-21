785f1e8648f45629a91e24cb4494e64b38e5eb18
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-21 16:40:26 +0100

    await?

--- main.js
+++ main.js
@@ -2,29 +2,42 @@ const controller = require('./output-hardware-controller');
 const { getDCRAvailableEvents } = require('./API-reader');
 const EventStates = require('./constants');
 
+function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 let stateList = [EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED];
 
-getDCRAvailableEvents("p-1-1").then((data) => {
+async function update() {
+    getDCRAvailableEvents("p-1-1").then((data) => {
 
-    console.log(data);
+        console.log(data);
 
-    data.forEach((event, i) => {
-        if (event[0] && event[1]) {
-            stateList[i] = EventStates.STATE_PENDING_INCLUDED;
-        } else if (event[0] && !event[1]) {
-            stateList[i] = EventStates.STATE_NOT_PENDING_INCLUDED;
-        } else if (!event[0] && event[1]) {
-            stateList[i] = EventStates.STATE_PENDING_EXCLUDED;
-        } else {
-            stateList[i] = EventStates.STATE_NOT_PENDING_EXCLUDED;
+        data.forEach((event, i) => {
+            if (event[0] && event[1]) {
+                stateList[i] = EventStates.STATE_PENDING_INCLUDED;
+            } else if (event[0] && !event[1]) {
+                stateList[i] = EventStates.STATE_NOT_PENDING_INCLUDED;
+            } else if (!event[0] && event[1]) {
+                stateList[i] = EventStates.STATE_PENDING_EXCLUDED;
+            } else {
+                stateList[i] = EventStates.STATE_NOT_PENDING_EXCLUDED;
+            }
+        });
+
+        for (let i = 0; i < 3; i++) {
+        controller.setState(i, stateList[i]);
         }
-    });
 
-    for (let i = 0; i < 3; i++) {
-     controller.setState(i, stateList[i]);
-    }
+    }); 
+}
 
-}); 
+while (true) {
+    update();
+    await wait(5000);
+}
+
+
 
 
 
