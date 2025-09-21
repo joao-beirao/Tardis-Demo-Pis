d788519e97f8d9caacd971b790dbcb0a6509b7bc
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-21 16:21:11 +0100

    API connection?

--- main.js
+++ main.js
@@ -2,13 +2,22 @@ const controller = require('./output-hardware-controller');
 const { getDCRAvailableEvents } = require('./API-reader');
 const EventStates = require('./constants');
 
+let stateList = [EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED];
 
-const PORT = 8080;
-
-getDCRAvailableEvents("p-1-1");
+let APIRRead = getDCRAvailableEvents("p-1-1");
+for (let i = 0; i < APIRRead.length; i++) {
+    if (APIRRead[i][0] && APIRRead[i][1]) {
+        stateList[i] = EventStates.STATE_PENDING_INCLUDED;
+    } else if (APIRRead[i][0] && !APIRRead[i][1]) {
+        stateList[i] = EventStates.STATE_PENDING_EXCLUDED;
+    } else if (!APIRRead[i][0] && APIRRead[i][1]) {
+        stateList[i] = EventStates.STATE_NOT_PENDING_INCLUDED;
+    } else {
+        stateList[i] = EventStates.STATE_NOT_PENDING_EXCLUDED;
+    }
+}
 
 for (let i = 0; i < 3; i++) {
-     controller.setState(i, EventStates.STATE_PENDING_INCLUDED);
-    // controller.setState(i, EventStates.STATE_NOT_PENDING_INCLUDED);
+     controller.setState(i, stateList[i]);
 }
 
