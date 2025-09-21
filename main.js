4b81431cd601c930765186c4a9b0c4c805bea80c
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-21 16:24:01 +0100

    foe each?

--- main.js
+++ main.js
@@ -5,17 +5,18 @@ const EventStates = require('./constants');
 let stateList = [EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED];
 
 let APIRRead = getDCRAvailableEvents("p-1-1");
-for (let i = 0; i < APIRRead.length; i++) {
-    if (APIRRead[i][0] && APIRRead[i][1]) {
+
+APIRRead.forEach((event, i) => {
+    if (event[0] && event[1]) {
         stateList[i] = EventStates.STATE_PENDING_INCLUDED;
-    } else if (APIRRead[i][0] && !APIRRead[i][1]) {
+    } else if (event[0] && !event[1]) {
         stateList[i] = EventStates.STATE_PENDING_EXCLUDED;
-    } else if (!APIRRead[i][0] && APIRRead[i][1]) {
+    } else if (!event[0] && event[1]) {
         stateList[i] = EventStates.STATE_NOT_PENDING_INCLUDED;
     } else {
         stateList[i] = EventStates.STATE_NOT_PENDING_EXCLUDED;
     }
-}
+});
 
 for (let i = 0; i < 3; i++) {
      controller.setState(i, stateList[i]);
