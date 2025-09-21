79a62cd6222d47864821b335c862bd52373011a8
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-21 16:32:31 +0100

    Then?

--- main.js
+++ main.js
@@ -4,21 +4,23 @@ const EventStates = require('./constants');
 
 let stateList = [EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED];
 
-let APIRRead = await getDCRAvailableEvents("p-1-1");
-console.log(APIRRead);
-/*
-APIRRead.forEach((event, i) => {
-    if (event[0] && event[1]) {
-        stateList[i] = EventStates.STATE_PENDING_INCLUDED;
-    } else if (event[0] && !event[1]) {
-        stateList[i] = EventStates.STATE_PENDING_EXCLUDED;
-    } else if (!event[0] && event[1]) {
-        stateList[i] = EventStates.STATE_NOT_PENDING_INCLUDED;
-    } else {
-        stateList[i] = EventStates.STATE_NOT_PENDING_EXCLUDED;
-    }
-});
-*/
+getDCRAvailableEvents("p-1-1").then((data) => {
+
+    console.log(data);
+
+    data.forEach((event, i) => {
+        if (event[0] && event[1]) {
+            stateList[i] = EventStates.STATE_PENDING_INCLUDED;
+        } else if (event[0] && !event[1]) {
+            stateList[i] = EventStates.STATE_PENDING_EXCLUDED;
+        } else if (!event[0] && event[1]) {
+            stateList[i] = EventStates.STATE_NOT_PENDING_INCLUDED;
+        } else {
+            stateList[i] = EventStates.STATE_NOT_PENDING_EXCLUDED;
+        }
+    });
+}); 
+
 for (let i = 0; i < 3; i++) {
      controller.setState(i, stateList[i]);
 }
