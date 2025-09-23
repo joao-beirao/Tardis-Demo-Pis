c82df904eb154b46d3def9ead9bef54e40931222
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-23 16:20:05 +0100

    no more loops

--- button-test.js
+++ button-test.js
@@ -1,9 +1,6 @@
 const buttons = require('./intput-hardware-controller');
 
-// Example: check button states every second
-setInterval(() => {
-  console.log("Button states:", buttons.buttonStates);
-}, 1000);
+
 
 process.on('SIGINT', () => {
   buttons.cleanup();
