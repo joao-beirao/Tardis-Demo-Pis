e1d9a8311bdadcca76d9ebfd81e59b8e6b8481b6
João Beirão <72351540+joao-beirao@users.noreply.github.com>
2025-09-27 16:46:44 +0100

    without 2 files?

--- main.js
+++ main.js
@@ -1,77 +1,18 @@
 const { getDCRAvailableEvents } = require('./API-reader');
 const EventStates = require('./constants');
-const { setOnButtonPress } = require('./input-hardware-controller');
+//const { setOnButtonPress } = require('./input-hardware-controller');
 const { executeConsume } = require('./EventController');
-const { Chip, Line } = require('node-libgpiod');
+const output = require('./output-hardware-controller');
 
-const chip = new Chip(4);
+let stateList = [EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED];
 
 async function wait(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-let stateList = [EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED, EventStates.STATE_NOT_PENDING_EXCLUDED];
-
-const led = [
-  [new Line(chip, 17), new Line(chip, 27), new Line(chip, 22)], //PENDING
-  [new Line(chip, 5), new Line(chip, 6), new Line(chip, 13)],   //INCLUDED
-];
-
-let ledStates = [
-  [0, 0, 0],
-  [0, 0, 0],
-];
-
-for (let i = 0; i < led.length; i++) {
-  for (let j = 0; j < led[i].length; j++) {
-    await led[i][j].requestOutputMode();
-  }
-}
-
-
-// * Change Led State
-function turnOn(i, j) {
-  if (ledStates[i][j] !== 1) {
-    led[i][j].setValue(1);
-    ledStates[i][j] = 1;
-  }
-}
-
-// * Change Led State
-function turnOff(i, j) {
-  if (ledStates[i][j] !== 0) {
-    led[i][j].setValue(0);
-    ledStates[i][j] = 0;
-  }
-}
-
-// * Set state based on EventStates
-async function setState(index, state) {
-  switch (state) {
-      case EventStates.STATE_NOT_PENDING_INCLUDED:
-          turnOff(0, index);
-          turnOn(1, index);
-          break;
-      case EventStates.STATE_NOT_PENDING_EXCLUDED:
-          turnOff(0, index);
-          turnOff(1, index);
-          break;
-      case EventStates.STATE_PENDING_INCLUDED:
-          turnOn(0, index);
-          turnOn(1, index);
-          break;
-      case EventStates.STATE_PENDING_EXCLUDED:
-          turnOn(0, index);
-          turnOff(1, index);
-          break;
-      default:
-          console.log('Unknown state');
-  }
-}
-
 
 async function update() {
-    await getDCRAvailableEvents("p-1-1").then( async (data) => {
+    await getDCRAvailableEvents("p-1-1").then( (data) => {
 
         console.log(data);
 
@@ -88,14 +29,14 @@ async function update() {
         });
 
         for (let i = 0; i < 3; i++) {
-          await setState(i, stateList[i]);
+          output.setState(i, stateList[i]);
         }
 
     }); 
 }
 
 async function main() {
-    setOnButtonPress(() => {executeConsume();});
+    //setOnButtonPress(() => {executeConsume();});
     while (true) {
         await update();
         await wait(1000);
